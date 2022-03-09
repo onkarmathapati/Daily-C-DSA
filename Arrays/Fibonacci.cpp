f14693b052dc9c83225df35edfee9c6e0c926e1b
unknown <mathapationkar8@gmail.com>
2022-03-09 18:02:13 +0530

    Fibonacci problem solved

---
+++ Arrays/Fibonacci.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+          int n1=0, n2=1, n3, i, n;
+          cout<<"enter the number of elements";
+          cin>>n;
+
+          cout<<n1<<" "<<n2<<" ";
+
+          for(int i =2; i<n; ++i){
+                    n3= n1+n2;
+                    cout<<n3<<" ";
+                    n1=n2;
+                    n2=n3;
+          }
+          return 0;
+}
